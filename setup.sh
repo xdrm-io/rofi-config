86b1d6f2b62ce1ee5d64fed2e28f6b1581379e45
xdrm <xdrm.dev@gmail.com>
2026-07-31 21:48:35 +0200

    chore: setup.sh formatting

--- setup.sh
+++ setup.sh
@@ -3,53 +3,53 @@
 ## Author : Aditya Shakya (adi1090x)
 ## Github : @adi1090x
 #
-## Installer Script
+## Installer Script (called from ../../../../install.sh)
 
 ## Colors ----------------------------
 Color_Off='\033[0m'
-BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m'
-BBlue='\033[1;34m'  BPurple='\033[1;35m' BCyan='\033[1;36m'  BWhite='\033[1;37m'
+BRed='\033[1;31m'
+BCyan='\033[1;36m'
 
 ## Directories ----------------------------
-DIR=`pwd`
+DIR=$(pwd)
 FONT_DIR="$HOME/.local/share/fonts"
 ROFI_DIR="$HOME/.config/rofi"
 
-# Install Fonts
-install_fonts() {
-	echo -e ${BBlue}"\n[*] Installing fonts..." ${Color_Off}
-	if [[ -d "$FONT_DIR" ]]; then
-		cp -rf $DIR/fonts/* "$FONT_DIR"
-	else
-		mkdir -p "$FONT_DIR"
-		cp -rf $DIR/fonts/* "$FONT_DIR"
-	fi
-	echo -e ${BYellow}"[*] Updating font cache...\n" ${Color_Off}
-	fc-cache
+step() {
+	# indented under "> install rofi"
+	echo -e "  ${BCyan}→${Color_Off} $*"
 }
 
-# Install Themes
+fail() {
+	echo -e "  ${BRed}✗${Color_Off} $*"
+	exit 1
+}
+
+install_fonts() {
+	step "fonts (~/.local/share/fonts)"
+	mkdir -p "$FONT_DIR"
+	cp -rf "$DIR"/fonts/* "$FONT_DIR"
+	step "font cache (fc-cache)"
+	fc-cache >/dev/null 2>&1
+}
+
 install_themes() {
 	if [[ -d "$ROFI_DIR" ]]; then
-		echo -e ${BPurple}"[*] Creating a backup of your rofi configs..." ${Color_Off}
-		mv "$ROFI_DIR" "${ROFI_DIR}.${USER}"
-	fi
-	echo -e ${BBlue}"[*] Installing rofi configs..." ${Color_Off}
-	{ mkdir -p "$ROFI_DIR"; cp -rf $DIR/files/* "$ROFI_DIR"; }
-
-	if [[ -f "$ROFI_DIR/config.rasi" ]]; then
-		echo -e ${BGreen}"[*] Successfully Installed.\n" ${Color_Off}
-		exit 0
+		bak="${ROFI_DIR}.${USER}"
+		step "backup (~/.config/rofi.${USER})"
+		rm -rf "$bak"
+		cp -a "$ROFI_DIR" "$bak"
 	else
-		echo -e ${BRed}"[!] Failed to install.\n" ${Color_Off}
-		exit 1
+		step "backup (skipped)"
+	fi
+	step "configs (~/.config/rofi)"
+	mkdir -p "$ROFI_DIR"
+	cp -rf "$DIR"/files/* "$ROFI_DIR"
+
+	if [[ ! -f "$ROFI_DIR/config.rasi" ]]; then
+		fail "rofi config missing after install"
 	fi
 }
 
-# Main
-main() {
-	install_fonts
-	install_themes
-}
-
-main
+install_fonts
+install_themes
